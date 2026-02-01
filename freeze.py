aa0457b3c34f4a3fb54ea7a70d701fb5ae3e543b
BarryS27 <238927804+BarryS27@users.noreply.github.com>
2026-02-01 22:14:51 +0000

    Revert "freeze"
    This reverts commit 0bda7821fac346382228597282134e05a7882950.

--- freeze.py
+++
@@ -1,9 +0,0 @@
-from flask_frozen import Freezer
-from app import app
-
-app.config['FREEZER_BASE_URL'] = '/Gitstarter/'
-
-freezer = Freezer(app)
-
-if __name__ == '__main__':
-    freezer.freeze()
